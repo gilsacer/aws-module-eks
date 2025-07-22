03b5deea20d21a3760ecd1e72a0a436528cbfe39
gilson-pires-ext_bjd <gilson.pires.ext@bancojohndeere.com.br>
2025-07-22 17:04:17 -0300

    feat: update tags

--- locals.tf
+++ locals.tf
@@ -3,7 +3,7 @@ locals {
     Project     = "DevOps-EKS"
     ManagedBy   = "Terraform"
     Owner       = "Team-Infra"
-    CreatedAt   = "2025-07-21"
+    CreatedAt   = "2025-07-22"
     Environment = "Dev"
   }
 }
